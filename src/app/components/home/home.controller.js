fe1ae811bdb2e84b101b8de79aa210050eb87f18
Nadar <A8859780@ext.saint-gobain.com>
2017-04-21 19:37:18 +0530

    amCharts integration

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -29,8 +29,8 @@ function HomeController($scope, $api) {
         $scope.project = data.task;
         $scope.projectweek = data.week;
     }
- 
-    $scope.getWeekNumber =function(d) {
+
+    $scope.getWeekNumber = function(d) {
         // Copy date so don't modify original
         d = new Date(d);
         d = new Date(+d);
@@ -43,11 +43,74 @@ function HomeController($scope, $api) {
         // Calculate full weeks to nearest Thursday
         var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
         // Return array of year and week number
-        return  'W'+ weekNo;
+        return 'W' + weekNo;
     }
 
     $scope.viewHome();
     $scope.viewProject();
+
+    var chartData = [{
+        "country": "USA",
+        "visits": 4252
+    }, {
+        "country": "China",
+        "visits": 1882
+    }, {
+        "country": "Japan",
+        "visits": 1809
+    }, {
+        "country": "Germany",
+        "visits": 1322
+    }, {
+        "country": "UK",
+        "visits": 1122
+    }, {
+        "country": "France",
+        "visits": 1114
+    }, {
+        "country": "India",
+        "visits": 984
+    }, {
+        "country": "Spain",
+        "visits": 711
+    }, {
+        "country": "Netherlands",
+        "visits": 665
+    }, {
+        "country": "Russia",
+        "visits": 580
+    }, {
+        "country": "South Korea",
+        "visits": 443
+    }, {
+        "country": "Canada",
+        "visits": 441
+    }, {
+        "country": "Brazil",
+        "visits": 395
+    }, {
+        "country": "Italy",
+        "visits": 386
+    }, {
+        "country": "Australia",
+        "visits": 384
+    }, {
+        "country": "Taiwan",
+        "visits": 338
+    }, {
+        "country": "Poland",
+        "visits": 328
+    }];
+
+    AmCharts.makeChart("chartdiv", {
+        "type": "serial",
+        "dataProvider": chartData,
+        "categoryField": "country",
+        "graphs": [{
+            "valueField": "visits",
+            "type": "column"
+        }]
+    });
 }
 
 angular
