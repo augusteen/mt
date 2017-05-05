10a57ee82996ce12f091916bbfe3393e04d4a5df
Nadar <A8859780@ext.saint-gobain.com>
2017-05-05 17:11:52 +0530

    temp

--- src/app/components/home/home.controller.js
+++ src/app/components/home/home.controller.js
@@ -1,12 +1,17 @@
 function HomeController($element, $scope, $api, $resource) {
     var self = this,
         chartData = [],
+        chart,
+        chartBase64,
         LeaveData = $resource(APIURL + 'api/timcardleave', null, {
             query: { method: 'POST' }
         });
+    //Public functions
     self.getWeekNumber = getWeekNumber;
     self.startCase = startCase;
-    //getLeave Data 
+    self.exportPdf = exportPdf;
+
+    //getLeave Data
     $scope.getLeave = function() {
         LeaveData.query({
             week: getLastFourMonday() //['2017-04-24', '2017-04-17']
@@ -44,7 +49,6 @@ function HomeController($element, $scope, $api, $resource) {
         data.week.push('Avg');
         $scope.week = data.week;
     }
-
     $scope.viewProject = function() {
         $api.resrc('api/timecardproject', {
             query: {
@@ -65,7 +69,7 @@ function HomeController($element, $scope, $api, $resource) {
         data.week.push('Avg');
         $scope.projectweek = data.week;
 
-        AmCharts.makeChart('chartdiv', {
+        chart = AmCharts.makeChart('chartdiv', {
             'type': 'serial',
             'theme': 'light',
             'dataProvider': chartData,
@@ -81,8 +85,78 @@ function HomeController($element, $scope, $api, $resource) {
                 'labelText': '[[value]]',
                 'fillAlphas': 0.8,
                 'fillColors': '#c5cae9'
-            }]
+            }],
+            'export': {
+                'enabled': true,
+                'menu': []
+            }
         });
+
+    }
+
+    function exportPdf() {
+        var doc = new jsPDF('p', 'pt'),
+            table1 = getColumnRows($scope.project, $scope.projectweek, true),
+            table2 = getColumnRows($scope.timecard, $scope.week, true),
+            table3 = getColumnRows($scope.leaveTask, $scope.leaveWeek);
+
+        doc.autoTable(table1.column, table1.row, {
+            margin: { top: 60 },
+            addPageContent: function(data) {
+                doc.text('Project Report', 40, 30);
+            }
+        });
+        chartImage();
+
+        doc.autoTable(table2.column, table2.row, {
+            margin: { top: 60 },
+            startY: doc.autoTable.previous.finalY + 10,
+            pageBreak: 'avoid'
+        });
+        doc.autoTable(table3.column, table3.row, {
+            margin: { top: 60 },
+            startY: doc.autoTable.previous.finalY + 10,
+            pageBreak: 'avoid'
+        });
+
+        doc.save('Project_Report.pdf');
+    }
+
+    function chartImage() {
+        chart.export.capture({}, function() {
+            this.toPNG({}, function(base64) {
+                // We have now a Base64-encoded image data
+                // which we can now transfer to server via AJAX
+                // i.e. jQuery.post( "saveimage.php", { "data": base64 } )
+                chartBase64 = base64;
+            });
+        });
+
+    }
+
+    function getColumnRows(row, col, percent) {
+        var columns = [],
+            rows = [];
+        _.forEach(row, function(task) {
+            _.forOwn(task.week, function(value, key) {
+                if (percent) {
+                    task.week[key] = _.round(value, 0) + '%';
+                } else {
+                    task.week[key] = value;
+                }
+            });
+            task.week.Name = _.startCase(task.Name);
+            rows.push(task.week);
+        });
+        columns.push({ title: 'Name', dataKey: 'Name' });
+        _.forEach(col, function(date) {
+            columns.push({
+                title: getWeekNumber(date),
+                dataKey: date
+            });
+        });
+
+        return { column: columns, row: rows };
     }
 
     function getWeekNumber(d) {
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,4 @@
-/// <reference path="modules/angular/index.d.ts" />
+/// <reference path="./modules/angular/index.d.ts" />
+/// <reference path="./modules/moment/moment.d.ts" />
+/// <reference path="./modules/amcharts/index.d.ts" />
+/// <reference path="./modules/jspdf/index.d.ts" />
